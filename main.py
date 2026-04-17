627d875d751973f2918cdc6b6ae49bb7a90d76de
Dilshod <ddilshod2197@gmail.com>
2026-04-17 16:30:07 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+matn = "SalomDunyoPython"
+katta_harflar = [harf for harf in matn if harf.isupper()]
+print(katta_harflar)
+
+sonlar = [-3, -1, 0, 2, 4, -5, 6]
+kvadratlar = [x**2 for x in sonlar if x > 0]
+print(kvadratlar)
